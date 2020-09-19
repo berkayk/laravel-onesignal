aa6f6fb1884b28e98052049668de2f40949bf912
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2020-09-19 22:21:48 +0100

    use dotenv for onesignal keys and id

--- config/onesignal.php
+++ config/onesignal.php
@@ -8,7 +8,7 @@ return array(
 	|
 	|
 	*/
-    'app_id' => 'YOUR-APP-ID-HERE',
+    'app_id' => env('ONESIGNAL_APP_ID'),
 
     /*
 	|--------------------------------------------------------------------------
@@ -18,6 +18,6 @@ return array(
     |
 	|
 	*/
-    'rest_api_key' => 'YOUR-REST-API-KEY-HERE',
-    'user_auth_key' => 'YOUR-USER-AUTH-KEY'
+    'rest_api_key' => env('ONESIGNAL_REST_API_KEY'),
+    'user_auth_key' => env('USER_AUTH_KEY')
 );
