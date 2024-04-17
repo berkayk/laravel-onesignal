224c5122849c853831307f5d98ae8d35964e89f2
Diogo Coutinho <diogo.coutinho@forpeople.io>
2024-04-17 11:52:01 -0300

    Removing a double "$" in a offset variable

--- src/OneSignalClient.php
+++ src/OneSignalClient.php
@@ -427,7 +427,7 @@ class OneSignalClient
         }
 
         if($offset) {
-            $endpoint.="&offset=".$$offset;
+            $endpoint.="&offset=".$offset;
         }
 
         return $this->get($endpoint);
